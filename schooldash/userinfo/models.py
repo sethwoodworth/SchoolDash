34b542a78e34009cd456975dca59017b868289a1
Seth Woodworth <seth@sethish.com>
2011-08-24 14:26:51 -0400

    add pseudo fkey to restrict access

--- schooldash/userinfo/models.py
+++ schooldash/userinfo/models.py
@@ -2,4 +2,6 @@ from django.db import models
 from userena.models import UserenaBaseProfile
 
 class UserProfile(UserenaBaseProfile):
-    fav_color = models.TextField(blank=True)
+    # Associate a user with a homeroom in userload.demographics.homeroom
+    # TODO: This should be a foreign key from here and demographics to a 'class' table
+    homeroom = models.TextField(blank=True)
